44685fb0db149d16d0636cd5eaae67aeb879d7ae
Mansoor Nisar <mansoornisar12@gmail.com>
2023-11-14 04:49:03 +0500

    [Calendar Screen -> Screen Created and Horizontal caledanr item work almost completed]

--- app/src/main/java/com/android/app/justbarit/presentation/feature_calendar/view/CalendarFragment.kt
+++ app/src/main/java/com/android/app/justbarit/presentation/feature_calendar/view/CalendarFragment.kt
@@ -1,23 +1,141 @@
 package com.android.app.justbarit.presentation.feature_calendar.view
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
 import com.android.app.justbarit.databinding.FragmentCalendarBinding
+import com.android.app.justbarit.domain.model.CalendarItem
+import com.android.app.justbarit.presentation.AppState
+import com.android.app.justbarit.presentation.common.ext.clickToAction
+import com.android.app.justbarit.presentation.common.ext.hideProgress
+import com.android.app.justbarit.presentation.common.ext.showProgress
+import com.android.app.justbarit.presentation.common.ext.smoothScrollToPositionWithUpdate
+import com.android.app.justbarit.presentation.feature_calendar.adapter.CalendarItemAdapter
+import com.android.app.justbarit.presentation.feature_calendar.adapter.calendarItemClick
+import com.android.app.justbarit.presentation.feature_calendar.viewmodel.CalendarViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class CalendarFragment : Fragment() {
     private lateinit var binding: FragmentCalendarBinding
+    private val viewModel: CalendarViewModel by viewModels()
+    private lateinit var calendarItemAdapter: CalendarItemAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentCalendarBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCalendarItems()
+        attachListeners()
+        observe()
+        viewModel.getListOfCalendarItems()
+    }
+
+    private fun observe() {
+        viewModel.apply {
+            lifecycleScope.launchWhenCreated {
+                calendarItems.collect {
+                    when (it) {
+                        is AppState.Loading -> {
+                            showProgress()
+                        }
+
+                        is AppState.Success<*> -> {
+                            hideProgress()
+                            calendarItemAdapter.setCalendarItems(it.response as ArrayList<CalendarItem>)
+                        }
+
+                        is AppState.Failure<*> -> {
+                            hideProgress()
+                        }
+
+                        else -> {}
+                    }
+                }
+            }
+        }
+    }
+
+    private fun attachListeners() {
+        binding.apply {
+
+            calendarArrowLeftLayout.clickToAction {
+                val currentPosition = calendarItemAdapter.selectedItemPosition()
+
+                if (currentPosition > 0) {
+                    //val scroller = binding.calendarItemRecyclerView.smoothScroll(currentPosition - 1)
+                    val smoothScroller =
+                        object : LinearSmoothScroller(binding.calendarItemRecyclerView.context) {
+                            override fun getVerticalSnapPreference(): Int {
+                                return SNAP_TO_START
+                            }
+
+                            override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics): Float {
+                                return 0.5f // Adjust the scrolling speed as needed
+                            }
+                        }
+
+                    smoothScroller.targetPosition = currentPosition - 1
+
+                    // Update the item position after starting the smooth scroll
+                    calendarItemAdapter.updateItemAtPosition(currentPosition - 1)
+
+                    (binding.calendarItemRecyclerView.layoutManager as LinearLayoutManager).startSmoothScroll(
+                        smoothScroller
+                    )
+                }
+            }
+
+            calendarArrowRightLayout.clickToAction {
+                val currentPosition = calendarItemAdapter.selectedItemPosition()
+                val lastPosition = (binding.calendarItemRecyclerView.layoutManager as LinearLayoutManager).itemCount - 1
+
+                if (currentPosition < lastPosition) {
+                    val smoothScroller =
+                        object : LinearSmoothScroller(binding.calendarItemRecyclerView.context) {
+                            override fun getVerticalSnapPreference(): Int {
+                                return SNAP_TO_START
+                            }
+
+                            override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics): Float {
+                                return 0.5f // Adjust the scrolling speed as needed
+                            }
+                        }
+
+                    smoothScroller.targetPosition = currentPosition + 1
+                    calendarItemAdapter.updateItemAtPosition(currentPosition + 1)
+
+                    // Update the item position after starting the smooth scroll
+
+                    (binding.calendarItemRecyclerView.layoutManager as LinearLayoutManager).startSmoothScroll(
+                        smoothScroller
+                    )
+                }
+            }
+        }
+    }
+
+    private fun initCalendarItems() {
+        calendarItemAdapter = CalendarItemAdapter(arrayListOf(), requireContext()).apply {
+            calendarItemClick = { calendarItem ->
+                calendarItem.selected = true
+                updateCalendarItem(calendarItem)
+            }
+        }
+        binding.calendarItemRecyclerView.adapter = calendarItemAdapter
     }
 
     companion object {
