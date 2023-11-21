5a0fc813b5c4044f659d99f1d6f99186ef8a2cb8
Mansoor Nisar <mansoornisar12@gmail.com>
2023-11-22 03:07:25 +0500

    [Bars called in SearchFragment]

--- app/src/main/java/com/android/app/justbarit/presentation/feature_search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/android/app/justbarit/presentation/feature_search/viewmodel/SearchViewModel.kt
@@ -6,8 +6,11 @@ import com.android.app.justbarit.R
 import com.android.app.justbarit.domain.model.Bar
 import com.android.app.justbarit.domain.model.Category
 import com.android.app.justbarit.domain.model.CategoryType
+import com.android.app.justbarit.domain.model.convertRemoteBarToLocalBarsList
+import com.android.app.justbarit.domain.usecases.GetBarsFromLocalDatabaseUseCase
 import com.android.app.justbarit.presentation.AppState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,18 +19,22 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject constructor() : ViewModel() {
+class SearchViewModel @Inject constructor(private val getBarsFromLocalDatabaseUseCase: GetBarsFromLocalDatabaseUseCase) : ViewModel() {
     private val _bars = MutableStateFlow<AppState>(AppState.Default)
     val bars: Flow<AppState> get() = _bars.asStateFlow()
 
 
     fun getListOfBars() {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             _bars.emit(AppState.Loading)
-            delay(2000)
-            val data = getHardCodeBars()
-            _bars.emit(AppState.Success(data))
+            try {
+                val barsList = getBarsFromLocalDatabaseUseCase.invoke()
+                _bars.emit(AppState.Success(barsList.convertRemoteBarToLocalBarsList()))
+            } catch (e: Exception) {
+                _bars.emit(AppState.Failure(e.message))
+            }
         }
+
     }
 
     private fun getHardCodeBars(): ArrayList<Bar> {
